86bb95750c0114fdae6cc0ec2afa113e770fe441
esziemba <97561230+esziemba@users.noreply.github.com>
2022-06-24 15:18:33 -0500

    Interface Improvments

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import { useEffect, useState } from 'react'
-import Gallery from './components/Gallery'
-import SearchBar from './components/SearchBar'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Gallery from './Components/Gallery'
+import SearchBar from './Components/SearchBar'
+import AlbumView from './Components/AlbumView'
+import ArtistView from './Components/ArtistView'
 
-function App(){
+function App() {
     let [search, setSearch] = useState('')
     let [message, setMessage] = useState('Search for Music!')
     let [data, setData] = useState([])
@@ -10,31 +13,46 @@ function App(){
     const API_URL = 'https://itunes.apple.com/search?term='
 
     useEffect(() => {
-        if(search) {
-        const fetchData = async () => {
-            document.title = `${search} Music`
-            const response = await fetch(API_URL + search)
-            const resData = await response.json()
-            if (resData.results.length > 0) {
-                setData(resData.results)
-            } else {
-                setMessage('Not Found')
+        if (search) {
+            const fetchData = async () => {
+                document.title = `${search} Music`
+                const response = await fetch(API_URL + search)
+                const resData = await response.json()
+                if (resData.results.length > 0) {
+                    setData(resData.results)
+                } else {
+                    setMessage('Not Found')
+                }
             }
+            fetchData()
         }
-        fetchData()
-    }
-}, [search])
+    }, [search])
 
     const handleSearch = (e, term) => {
         e.preventDefault()
         setSearch(term)
     }
-    
+
     return (
         <div>
-            <SearchBar handleSearch = {handleSearch} />
-            {message}
-            <Gallery data={data} />
+            return (
+            <div>
+                {message}
+                <Router>
+                    <Routes>
+                        <Route path="/" element={
+                            <Fragment>
+                                <SearchBar handleSearch={handleSearch} />
+                                <Gallery data={data} />
+                            </Fragment>
+                        } />
+                        <Route path="/album/:id" element={<AlbumView />} />
+                        <Route path="/artist/:id" element={<ArtistView />} />
+                    </Routes>
+                </Router>
+            </div>
+            )
+
         </div>
     )
 }
---
+++ src/Components/ArtistView.js
@@ -0,0 +1,56 @@
+// These components will be making separate API calls from the app
+// component to serve specific data about our artist
+import { useState, useEffect } from 'react'
+import { useParams, Link } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
+
+function ArtistView() {
+    const navigate = useNavigate()
+    const { id } = useParams()
+    const [ artistData, setArtistData ] = useState([])
+
+    useEffect(() => {
+        const API_URL = 'http://localhost:4000/album/${id}'
+        const fetchData = async () => {
+            const response = await fetch(API_URL)
+            const resData = await response.json()
+            setArtistData(resData.results)
+        }
+        fetchData()
+    }, [id])
+
+    const justAlbums = artistData.filter(entry => entry.collectionType === 'Album')
+
+    const renderAlbums = justAlbums.map((album, i) => {
+        return (
+            <div>
+                <Link to={`/album/${album.collectionId}`}>
+                    <p>{album.collectionName}</p>
+                </Link>
+            </div>
+        )
+    })
+
+    const navButtons = () => {
+        return (
+            <div>
+                <Button onClick={ () => navigate(-1)}>Back</Button>
+                |
+                <Button onClick={ () => navigate('/')}>Home</Button>
+            </div>
+        )
+    }
+
+    return (
+        <div>
+            {artistData.length > 0 ? <h2>{artistData[0].artistName}</h2> : <h2>Loading...</h2>}
+            {navButtons()}
+            {renderAlbums}
+        </div>
+    )
+}
+
+
+
+
+export default ArtistView
